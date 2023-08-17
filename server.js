6e0eac2ed0540047dabe178e4cc3d8c62e4c932c
Iryna Kutsevol <117863253+irkutsio@users.noreply.github.com>
2023-08-17 18:30:02 +0300

    move DB_HOST

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
-const DB_HOST =
-	'mongodb+srv://Ira:9CsdFvTxZifadWN0@cluster0.0on18yj.mongodb.net/books_db?retryWrites=true&w=majority';
+
+const DB_HOST = require('./config.js')
 
 const app = require('./app');
 mongoose
 	.connect(DB_HOST)
 	.then(() => {
 		app.listen(3000, () => {
-			console.log('Server running. Use our API on port: 3000');
+			console.log('Database connection successful');
 		});
 	})
 	.catch(error => {
